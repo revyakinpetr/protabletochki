e06af29f8ea362fba13a6b447eb1d31f7bd8ad3c
Valeria Stromtsova <v.stromtsova@yandex.ru>
2019-12-11 10:34:40 +0300

    Made tokenizer work with files in data directory

--- tokenizator.py
+++ tokenizator.py
@@ -1,19 +1,22 @@
 from json_utils import get_json_from_file, save_json_to_file
 import nltk
+import os
+from os.path import isfile, join
+from parsers.parse_drug_names import parse_drug_names
 import string
 from nltk.corpus import stopwords, wordnet
-from nltk.stem import WordNetLemmatizer 
+from nltk.stem import WordNetLemmatizer
 
-DEFAULT_DATA_SOURCE = 'data/drugs_en.json'
-DEFAULT_DATA_DESTINATION = 'data/drugs_token.json'
+DATA_DIR = os.getcwd() + '/data/'
+DRUG_FILES = [f for f in os.listdir(DATA_DIR) if isfile(join(DATA_DIR, f))]
+
+DRUG_NAMES = parse_drug_names(drugs_list_filename=DATA_DIR + 'drugs_list.json')
 
 REVIEW_FIELDS_TO_TOKENIZE = (
-    'comment_plus',
-    'comment_minus',
     'comment'
 )
 
- 
+
 def get_wordnet_pos(word):
     """Получение части речи для лемматизации"""
     tag = nltk.pos_tag([word])[0][1][0].upper()
@@ -25,33 +28,32 @@ def get_wordnet_pos(word):
 
 
 def normalize_comments(
-        drugs:dict,
+        reviews: dict,
 ):
     """Нормализация коментариев посредством удаления знаков пунктации, стоп слов, а также токенизация и лемматизация"""
     lemmatizer = WordNetLemmatizer()
     punctuation = string.punctuation
     punctuation += "''``"
-    for drug in drugs:
-        for review in drug['reviews']:
-            for field in review.keys():
-                if field in REVIEW_FIELDS_TO_TOKENIZE:
-                    review[field] = nltk.word_tokenize(review[field])
-                    review[field] = [i for i in review[field] if ( i not in punctuation )]
-                    review[field] = [i for i in review[field] if ( i not in stopwords.words('english') )]
-
-                    review[field] = [lemmatizer.lemmatize(i, get_wordnet_pos(i)) for i in review[field]]
-    return drugs
+    for review in reviews:
+        review['comment'] = nltk.word_tokenize(review['comment'])
+        review['comment'] = [i for i in review['comment'] if (i not in punctuation)]
+        review['comment'] = [i for i in review['comment'] if (i not in stopwords.words('english'))]
+        review['comment'] = [lemmatizer.lemmatize(i, get_wordnet_pos(i)) for i in review['comment']]
+    return reviews
 
 
 if __name__ == "__main__":
 
-    drugs_json = get_json_from_file(
-        filename=DEFAULT_DATA_SOURCE
-    )
+    for drug_name in DRUG_NAMES:
+        for drug_file in DRUG_FILES:
+            if drug_file.startswith('en_'+drug_name):
+                reviews_json = get_json_from_file(
+                    filename=DATA_DIR+drug_file
+                )
 
-    normalize_comments(drugs_json)
+                normalize_comments(reviews_json)
 
-    save_json_to_file(
-        filename=DEFAULT_DATA_DESTINATION,
-        data=drugs_json,
-    )
+                save_json_to_file(
+                    filename=DATA_DIR+'token_' + drug_file,
+                    data=reviews_json,
+                )
