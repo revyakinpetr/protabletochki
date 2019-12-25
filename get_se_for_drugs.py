ffe1282a7706a8f3f50dc5e83bebe3ce5269c245
revyakinpetr <revyakinpa@ya.ru>
2019-12-25 03:32:04 +0300

    #14: can save drugs without duplicate info

--- get_se_for_drugs.py
+++ get_se_for_drugs.py
@@ -16,7 +16,7 @@ DRUG_FILES = [
 ]
 DICT_WORDS = get_json_from_file(DATA_DIR + 'drugs_dict_words_beta.json')
 
-SAVE_FILENAME = '/data/drug_side_effects_dicted.json'
+SAVE_FILENAME = '/data/drug_side_effects_dicted_novk.json'
 
 
 with open(DATA_DIR+'meddra_all_se.txt', 'r') as f:
@@ -26,6 +26,12 @@ with open(DATA_DIR+'meddra_all_se.txt', 'r') as f:
 def get_drug_name_from_filename(filename):
     return filename.split('.')[0].split('_')[2]
 
+
+def get_index_of_drug_in_array(array, drug_name):
+    for i in range(len(array)):
+        if array[i]['drug'] == drug_name:
+            return i
+    return -1
 
 def hasReviewDictWords(review_words):
     for word in DICT_WORDS:
@@ -61,9 +67,21 @@ if __name__ == "__main__":
     drug_names = parse_drug_names(path_to_drugs)
 
     for drug_file in DRUG_FILES:
-        if drug_file.startswith('token_en_'):
-            drug_side_effects.append(work_with_drug_file(drug_file))
+        if drug_file.startswith('token_en_') and 'vk' not in drug_file:
+            drug_name = get_drug_name_from_filename(drug_file)
+            drug_file_se = work_with_drug_file(drug_file)
+            index = get_index_of_drug_in_array(drug_side_effects, drug_name)
+            if index != -1:
+                drug_side_effects[index]['side_effects'] += drug_file_se['side_effects']
+                drug_side_effects[index]['side_effects'] = list(set(drug_side_effects[index]['side_effects']))
+            else:
+                drug_side_effects.append(drug_file_se)
 
     filename = get_correct_path(os.getcwd() + SAVE_FILENAME)
-    pprint(drug_side_effects)
+
+
+    for drug in drug_side_effects:
+        if len(drug['side_effects']) != 0:
+            pprint(drug)
+
     save_json_to_file(filename, drug_side_effects)
